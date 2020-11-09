5a7f65ea79ba3cce8deb14358bb88cdbfbf8d65c
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-10 00:19:18 +0530

    Added: Test case to find the cheapest hotel for a given date range based on weekday and weekend

--- src/test/java/com/blz/hotel_reservation/HotelReservationTest.java
+++ src/test/java/com/blz/hotel_reservation/HotelReservationTest.java
@@ -1,9 +1,12 @@
 package com.blz.hotel_reservation;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -12,6 +15,9 @@ import org.junit.Test;
 public class HotelReservationTest {
 
 	private static HotelReservation obj;
+
+	static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dMMMyyyy", Locale.ENGLISH);
+	LocalDate date;
 
 	@BeforeClass
 	public static void createHotelReservationSystemObj() {
@@ -26,15 +32,29 @@ public class HotelReservationTest {
 	}
 
 	@Test
-	public void givenDateRange_FindCheapestRate_AndReturnHotel() {
-		List<String> dateList = new ArrayList<>();
-		dateList.add("10sep2020");
-		dateList.add("11sep2020");
-		int totaldays = dateList.size();
-		int cheapestRate = obj.getCheapestRate(totaldays);
-		String cheapestHotel = obj.getCheapestHotel(cheapestRate);
-		assertEquals("Lakewood", cheapestHotel);
-		System.out.println(cheapestHotel + ", Total Rate : $" + cheapestRate);
-
+	public void givenDateRange_FindCheapestRate_ShouldReturnHotel() {
+		String date1 = "10Sep2020";
+		String date2 = "11Sep2020";
+		List<Integer> dates = new ArrayList<>();
+		dates.add(LocalDate.parse(date1, formatter).getDayOfWeek().getValue());
+		dates.add(LocalDate.parse(date2, formatter).getDayOfWeek().getValue());
+		int cheapestRate = obj.getCheapestRate(dates);
+		List<String> cheapestHotel = obj.getCheapestHotel(cheapestRate);
+		assertTrue(cheapestHotel.contains("Lakewood"));
+		System.out.println("Cheapest Hotel : " + cheapestHotel + ", Total charges: $" + cheapestRate);
 	}
+
+	@Test
+	public void givenWeekdayAndWeekendDay_FindCheapestRate_ShouldReturnHotels() {
+		String date1 = "11Sep2020";
+		String date2 = "12Sep2020";
+		List<Integer> dates = new ArrayList<>();
+		dates.add((LocalDate.parse(date1, formatter)).getDayOfWeek().getValue());
+		dates.add((LocalDate.parse(date2, formatter)).getDayOfWeek().getValue());
+		int cheapestRate = obj.getCheapestRate(dates);
+		List<String> cheapestHotel = obj.getCheapestHotel(cheapestRate);
+		assertTrue(cheapestHotel.contains("Lakewood"));
+		System.out.println(cheapestHotel + " with Total charges : $" + cheapestRate);
+	}
+
 }
