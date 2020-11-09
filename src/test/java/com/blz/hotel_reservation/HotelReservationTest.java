c4f8a3512b8b4c5469cbf6796a88491a08b75c2e
Sivakumar-U <usivakumarreddy@gmail.com>
2020-11-09 19:46:54 +0530

    Added: Test case to find cheapest hotel for a given date range

--- src/test/java/com/blz/hotel_reservation/HotelReservationTest.java
+++ src/test/java/com/blz/hotel_reservation/HotelReservationTest.java
@@ -1,5 +1,10 @@
 package com.blz.hotel_reservation;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -18,5 +23,17 @@ public class HotelReservationTest {
 	public void givenHotelDetails_WhenAdded_ShouldReturnSize() {
 		int numOfHotels = obj.getSize();
 		Assert.assertEquals(3, numOfHotels);
+	}
+
+	@Test
+	public void givenDateRange_FindCheapestRate_AndReturnHotel() {
+		List<String> dateList = new ArrayList<>();
+		dateList.add("10sep2020");
+		dateList.add("11sep2020");
+		int totaldays = dateList.size();
+		int cheapestRate = obj.getCheapestRate(totaldays);
+		String cheapestHotel = obj.getCheapestHotel(cheapestRate);
+		assertEquals("Lakewood", cheapestHotel);
+		System.out.println(cheapestHotel + ", Total Charges: $" + cheapestRate);
 	}
 }
